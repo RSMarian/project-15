e6554e309c62366616dc5598e4f10ee33cf6e7a5
RSMarian <marianstefan1499@gmail.com>
2022-06-03 13:34:10 +0300

    added functionality

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
-const modalBtn = document.querySelector('modal-btn');
-const modalOverlay = document.querySelector('modal-overlay');
-const closeBtn = document.querySelector('close-btn');
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
 
 modalBtn.addEventListener('click', function(){
-    if()
-})
+    modalOverlay.classList.add('open-modal')
+});
+
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal')
+});
